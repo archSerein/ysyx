acf16c86cdbf4ee3060addd1d3dcbda4210e1862
crazyofcode <1710338943@qq.com>
2023-11-26 18:29:17 +0800

    update

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,33 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vtop.h>
+
+static TOP_NAME top;
+void nvboard_bind_all_pins(Vtop* top);
+
+void single_cycle() {
+  top.clk = 0;
+  top.eval();
+  top.clk = 1;
+  top.eval();
+}
+
+void reset(int n) {
+  top.rst = 1;
+  while (n-- > 0)
+    single_cycle();
+  top.rst = 0;
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
+  nvboard_bind_all_pins(&top);
+  nvboard_init();
+  
+  reset(10);
+  
+  while(1)
+  {
+    nvboard_update();
+    single_cycle();
+  }
   return 0;
 }
