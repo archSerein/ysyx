acbf3264ec2a183e9ed88694217a1ab7d34fc1fc
crazyofcode <1710338943@qq.com>
2024-03-13 14:42:53 +0800

    NJU-ProjectN/abstract-machine ics2023 initialized
    NJU-ProjectN/abstract-machine c383159000a2975dabde43f221deae3d54bc48b0 am,npc: add uart config function

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,33 +1,39 @@
-#include <nvboard.h>
+// #include <nvboard.h>
 #include <Vtop.h>
+#include <iostream>
 
 static TOP_NAME top;
-void nvboard_bind_all_pins(Vtop* top);
+int e = 0;
+// void nvboard_bind_all_pins(Vtop* top);
 
 void single_cycle() {
-  top.clk = 0;
-  top.eval();
-  top.clk = 1;
-  top.eval();
+    top.clk = 0;
+    top.eval();
+    top.clk = 1;
+    top.eval();
 }
 
 void reset(int n) {
-  top.rst = 1;
-  while (n-- > 0)
-    single_cycle();
-  top.rst = 0;
+    top.rst = 1;
+    while (n-- > 0)
+        single_cycle();
+    top.rst = 0;
 }
 
+extern "C" void ending(int num) { e = num; }
+
 int main() {
-  nvboard_bind_all_pins(&top);
-  nvboard_init();
-  
-  reset(10);
-  
-  while(1)
-  {
-    nvboard_update();
-    single_cycle();
-  }
-  return 0;
+    // nvboard_bind_all_pins(&top);
+    // nvboard_init();
+
+    reset(10);
+
+    while (1) {
+        // nvboard_update();
+        if (e)
+            break;
+        single_cycle();
+        printf("%d\n", top.out);
+    }
+    return 0;
 }
