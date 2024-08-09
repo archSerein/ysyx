2fead701a2f539b81718bb5ac68fbf46b68d231a
crazyofcode <1710338943@qq.com>
2024-08-09 09:39:05 +0800

    Fixing the bool module and successfully using yosyszoo synthesis

--- npc/vsrc/bool.v
+++ npc/vsrc/bool.v
@@ -9,12 +9,23 @@ module bool(
     wire [31:0] b = bool_b_i;
     wire [3:0] fn = bool_op_i;
     wire [31:0] S;
+    // genvar i;
+
+    // generate
+    //     for(i = 0; i < 32; i = i + 1)
+    //     begin   :   bool_gen
+    //         MuxKey mux (.key({a[i], b[i]}), .value(fn), .out(S[i]));
+    //     end
+    // endgenerate
     genvar i;
 
     generate
         for(i = 0; i < 32; i = i + 1)
-        begin   :   bool
-            MuxKey mux (.out(S[i]), .key({a[i], b[i]}), .value(fn));
+        begin   :   bool_gen
+            assign S[i] =   {a[i], b[i]} == 2'b00 & fn[0] |
+                            {a[i], b[i]} == 2'b01 & fn[1] |
+                            {a[i], b[i]} == 2'b10 & fn[2] |
+                            {a[i], b[i]} == 2'b11 & fn[3];
         end
     endgenerate
 
